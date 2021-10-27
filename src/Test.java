da8ab256a20170cc97599cb6cf1b9d80721db390
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-10-26 20:55:37 -0700

    first commit

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Thiwanka
+ */
+public class Test {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
+abstract class Shape{
+    Clour clour;
+    public abstract void draw();
+}
+abstract class Clour{
+    public abstract void fill();
+}
