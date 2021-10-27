633679103ed46bddffeb5f84cdc3bc6341c56829
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-10-26 21:02:03 -0700

    first commit

--- src/Test.java
+++ src/Test.java
@@ -14,6 +14,43 @@ abstract class Shape{
     Clour clour;
     public abstract void draw();
 }
-abstract class Clour{
+
+class Circle extends Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Circle");
+    }
+    
+}
+
+class Rectangale extends Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle");
+    }
+    
+}
+
+interface Clour{
     public abstract void fill();
+}
+
+class Red implements Clour{
+
+    @Override
+    public void fill() {
+        System.out.println("FIll Red Clour");
+    }
+    
+}
+
+class Blue implements Clour{
+
+    @Override
+    public void fill() {
+        System.out.println("FIll Blue Clour");
+    }
+    
 }
